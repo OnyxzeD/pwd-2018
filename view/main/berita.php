2d97823364e2dc5bc7c3ad549a417bf00b478957
arunstop <arunstop@gmail.com>
2018-07-19 00:17:55 +0700

    Add files via upload

--- view/main/berita.php
+++ view/main/berita.php
@@ -23,61 +23,25 @@ $base_url_index = 'http://localhost/CRUD-NATIVE/index.php?';
         </div>
     </div>
 </div>
-<div class="py-3">
+<?php
+while ($berita = mysqli_fetch_assoc($data)) {?>
+    <div class="py-3">
     <div class="container">
         <div class="row">
             <div class="col-md-5 order-2 order-md-1">
                 <img class="img-fluid d-block" src="https://pingendo.com/assets/photos/wireframe/photo-1.jpg"></div>
             <div class="col-md-7 order-1 order-md-2">
-                <h3>Pendaftaran siswa</h3>
-                <p class="my-3">Both columns have a
-                    <i>order-n&nbsp;</i>class. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                    exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                    nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                    mollit anim id est laborum.</p>
+                <h3><?php echo $berita['title'];?></h3>
+                <p class="my-3"><?php echo $berita['content'];?></p>
+                <p class="my-3"><?php echo $berita['created_at'];?></p>
             </div>
         </div>
     </div>
 </div>
-<div class="py-3">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-5 order-2 order-md-1">
-                <img class="img-fluid d-block" src="https://pingendo.com/assets/photos/wireframe/photo-1.jpg"></div>
-            <div class="col-md-7 order-1 order-md-2">
-                <h3>UASBN 2018</h3>
-                <p class="my-3">Both columns have a
-                    <i>order-n&nbsp;</i>class. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                    exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                    nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                    mollit anim id est laborum.</p>
-            </div>
-        </div>
-    </div>
-</div>
-<div class="py-3">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-5 order-2 order-md-1">
-                <img class="img-fluid d-block" src="https://pingendo.com/assets/photos/wireframe/photo-1.jpg"></div>
-            <div class="col-md-7 order-1 order-md-2">
-                <h3>Switching columns under
-                    <i>md</i> breakpoint</h3>
-                <p class="my-3">Both columns have a
-                    <i>order-n&nbsp;</i>class. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                    exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                    nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                    mollit anim id est laborum.</p>
-            </div>
-        </div>
-    </div>
-</div>
+<?php
+}
+
+?>
 <script src="<?php echo $base_url ?>assets/js/jquery-3.2.1.slim.min.js"
         integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
         crossorigin="anonymous"></script>
